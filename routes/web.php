d3b83eabc6ed249a6847b328f2f266a0203f3296
Vo Xuan Tan <tanvophuyen@gmail.com>
2026-04-07 12:55:59 +0700

    thu nghiem CI/CD cuoi ky

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ Route::get('/',[webCoffeController::class , 'index']);
 Route::get('/about',[webCoffeController::class, 'about']);
 Route::get('/menu', [webCoffeController::class, 'menu']);
 Route::get('/beanstory', [webCoffeController::class, 'beanstory']);
-// Route::get('/contact', [webCoffeController::class, 'contact']);
-// Route::get('/item/{id}', [webCoffeController::class, 'productDetail']);
+Route::get('/contact', [webCoffeController::class, 'contact']);
+Route::get('/item/{id}', [webCoffeController::class, 'productDetail']);
